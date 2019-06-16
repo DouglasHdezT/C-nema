e6c37d5b3b798061d02879881e72557fb0339ec7
DouglasHdezT <00004516@uca.edu.sv>
2019-06-15 21:41:29 -0600

    Audit Clases Implemented

--- src/com/kdc/cnema/domain/audit/DeptoAudit.java
+++ src/com/kdc/cnema/domain/audit/DeptoAudit.java
@@ -7,27 +7,21 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 /**
- * Clase que mapea la entidad "cuentas_auditoria" en la base de datos del proyecto.
+ * Clase que mapea la entidad "depto_auditoria" en la base de datos del proyecto.
  * @author DeusHdezT
  * @version 1.0
  */
-@Entity(name = "cuentas_auditoria")
-public class ProfileAudit {
-	
+@Entity(name = "depto_auditoria")
+public class DeptoAudit {
+
 	@Id
-	@Column(name = "id_cuenta")
+	@Column(name = "id_categoria_auditoria")
 	private Integer _id;
 	
 	@Column(name = "usuario_modificacion")
 	private String userModifier;
 	
 	@Column(name = "fecha_modificacion")
-	private Date modificationDate;
-	
-	@Column(name = "tipo_accion_estado")
-	private Boolean stateChanged;
-	
-	@Column(name = "razon")
-	private String argument;
+	private Date modificationDate; 
 	
 }
