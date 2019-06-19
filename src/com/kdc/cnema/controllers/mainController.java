28d2db6914942d38e0c7d4761b08762c6ceda90d
DouglasHdezT <00004516@uca.edu.sv>
2019-06-18 21:51:27 -0600

    Fixing dependencies

---
+++ src/com/kdc/cnema/controllers/mainController.java
@@ -0,0 +1,22 @@
+package com.kdc.cnema.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kdc.cnema.domain.User;
+
+@Controller
+public class mainController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String main(@ModelAttribute User user) {
+		
+		
+		
+		return "Funcionando";
+	}
+	
+}
