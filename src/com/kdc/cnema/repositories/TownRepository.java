99f14b1ce8b5f558c85e490e7ce49bfbdbdd7147
CarlosDubon <00058016@uca.edu.sv>
2019-06-18 21:29:09 -0600

    fix town repo

--- src/com/kdc/cnema/repositories/TownRepository.java
+++ src/com/kdc/cnema/repositories/TownRepository.java
@@ -2,6 +2,8 @@ package com.kdc.cnema.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TownRepository extends JpaRepository<TownRepository, Integer>{
+import com.kdc.cnema.domain.Town;
+
+public interface TownRepository extends JpaRepository<Town, Integer>{
 
 }
