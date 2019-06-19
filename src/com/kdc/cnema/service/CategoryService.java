9b8572a7bc7d5fd47b2289fa43c2192470bf0d1e
kbeam <kbeam@DESKTOP-KDBLCP8>
2019-06-18 21:04:50 -0600

    Commit service

--- src/com/kdc/cnema/service/CategoryService.java
+++ src/com/kdc/cnema/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.kdc.cnema.service;
 
+import java.util.List;
+
 import org.springframework.dao.DataAccessException;
 
 import com.kdc.cnema.domain.Category;
@@ -7,4 +9,8 @@ import com.kdc.cnema.domain.Category;
 public interface CategoryService {
 	
 	public Category findOneById() throws DataAccessException;
+	List<Category> findAll() throws DataAccessException;
+	Category save(Category category) throws DataAccessException;
+	void deleteById(Integer id) throws DataAccessException;
+
 }
--- src/com/kdc/cnema/service/implementation/CountryServiceImpl.java
+++ src/com/kdc/cnema/service/implementation/CountryServiceImpl.java
@@ -1,10 +1,38 @@
 package com.kdc.cnema.service.implementation;
 
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.kdc.cnema.domain.Country;
 import com.kdc.cnema.service.CountryService;
 
 @Service
 public class CountryServiceImpl implements CountryService{
+
+	@Override
+	public Country findOneById() throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Country> findAll() throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Country save(Country country) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteById(Integer id) throws DataAccessException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
