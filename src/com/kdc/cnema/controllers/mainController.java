ab61e106cb4b120aebed801b1b84e340d578986c
DouglasHdezT <00004516@uca.edu.sv>
2019-06-18 22:19:29 -0600

    Fixing controller debug

--- src/com/kdc/cnema/controllers/mainController.java
+++ src/com/kdc/cnema/controllers/mainController.java
@@ -1,11 +1,8 @@
 package com.kdc.cnema.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.kdc.cnema.domain.User;
 
 @Controller
 public class mainController {
