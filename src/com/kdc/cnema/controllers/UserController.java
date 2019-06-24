2da97ecc64f4300b398090fed5bc123af513602f
DouglasHdezT <00004516@uca.edu.sv>
2019-06-23 19:09:37 -0600

    Adding cross origin to usercontroller

--- src/com/kdc/cnema/controllers/UserController.java
+++ src/com/kdc/cnema/controllers/UserController.java
@@ -6,20 +6,28 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kdc.cnema.domain.Reservation;
 import com.kdc.cnema.domain.User;
 import com.kdc.cnema.exceptions.MalformedAuthHeader;
 import com.kdc.cnema.service.UserService;
 import com.kdc.cnema.utils.JwtPayload;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	/*
+	 * Admin methods
+	 */
 	
 	@RequestMapping("/users/all")
 	public ResponseEntity<List<User>> getAllCategories(@RequestHeader("Authorization") String authHeader){
@@ -53,6 +61,44 @@ public class UserController {
 		return new ResponseEntity<List<User>>(
 				users,
 				code);
+	}
+	
+	
+	/*
+	 * Users methods
+	 */
+	
+	@RequestMapping(value = "/users/reservations", method = RequestMethod.POST)
+	public ResponseEntity<List<Reservation>> getAllReservationPerUser(@RequestHeader("Authorization") String authHeader){
+		List<Reservation> reservations = new ArrayList<>();
+		HttpStatus code = HttpStatus.BAD_REQUEST;
+		
+		try {
+			JwtPayload.validateToken(authHeader);
+			JwtPayload payload = JwtPayload.decodeToken(authHeader.substring(7));
+			
+			User user = userService.findOneById(Integer.parseInt(payload.getUid()));
+			
+			if(user == null) {
+				code = HttpStatus.NOT_FOUND;
+			}else {
+				reservations = user.getReservations();
+				code = HttpStatus.OK;
+			}
+			
+			
+		}catch (io.jsonwebtoken.SignatureException e) {
+			code = HttpStatus.FORBIDDEN;
+		}catch (io.jsonwebtoken.MalformedJwtException e) {
+			code = HttpStatus.FORBIDDEN;
+		}catch (MalformedAuthHeader e) {
+			code = HttpStatus.FORBIDDEN;
+		}catch (Exception e) {
+			e.printStackTrace();
+			code = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		
+		return new ResponseEntity<List<Reservation>>(reservations, code);
 	}
 	
 }
